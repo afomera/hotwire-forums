40b49da26624830a2ef2bbe51e6057bea9ec0f86
Andrea Fomera <afomera@hey.com>
2021-02-23 22:18:16 -0700

    Installing bootstrap and adding a main route

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,7 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+  Popper: ['popper.js', 'default']
+}))
 
 module.exports = environment
