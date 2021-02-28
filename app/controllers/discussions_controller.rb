17662484d64646234960c828d90c8ab9b1f14626
Andrea Fomera <afomera@hey.com>
2021-02-28 15:43:18 -0700

    Create discussions from the UI

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -4,4 +4,26 @@ class DiscussionsController < ApplicationController
   def index
     @discussions = Discussion.all
   end
+
+  def new
+    @discussion = Discussion.new
+  end
+
+  def create
+    @discussion = Discussion.new(discussion_params)
+
+    respond_to do |format|
+      if @discussion.save
+        format.html { redirect_to discussions_path, notice: "Discussion created" }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def discussion_params
+    params.require(:discussion).permit(:name, :closed, :pinned)
+  end
 end
