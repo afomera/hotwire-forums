5f335191b8c8ebf862a7370fdadb8bd6e0f9070f
Andrea Fomera <afomera@hey.com>
2021-02-28 12:19:40 -0700

    Per request Current attribute support to set Current.user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_current_user, if: :user_signed_in?
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
+
+  private
+
+  def set_current_user
+    Current.user = current_user
   end
 end
