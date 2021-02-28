6b07c35561ff4ce7363e2aaa3c4086bbf708f8c0
Andrea Fomera <afomera@hey.com>
2021-02-28 16:02:03 -0700

    Edit discussions

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -1,5 +1,6 @@
 class DiscussionsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_discussion, only: [:edit, :update]
 
   def index
     @discussions = Discussion.all
@@ -21,9 +22,26 @@ class DiscussionsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @discussion.update(discussion_params)
+        format.html { redirect_to discussions_path, notice: "Discussion updated" }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
 
   def discussion_params
     params.require(:discussion).permit(:name, :closed, :pinned)
+  end
+
+  def set_discussion
+    @discussion = Discussion.find(params[:id])
   end
 end
