47dadc8a0f6514b0cf49640703494b0b61e1b6a2
Andrea Fomera <afomera@hey.com>
2021-02-28 17:42:46 -0700

    Real time discussion functional

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -3,7 +3,7 @@ class DiscussionsController < ApplicationController
   before_action :set_discussion, only: [:show, :edit, :destroy, :update]
 
   def index
-    @discussions = Discussion.all
+    @discussions = Discussion.all.order(updated_at: :desc)
   end
 
   def show
@@ -31,6 +31,8 @@ class DiscussionsController < ApplicationController
   def update
     respond_to do |format|
       if @discussion.update(discussion_params)
+        @discussion.broadcast_replace(partial: "discussions/header", locals: { discussion: @discussion })
+
         format.html { redirect_to @discussion, notice: "Discussion updated" }
       else
         format.html { render :edit, status: :unprocessable_entity }
--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -3,6 +3,10 @@ class Discussion < ApplicationRecord
 
   validates :name, presence: true
 
+  after_create_commit -> { broadcast_prepend_to "discussions" }
+  after_update_commit -> { broadcast_replace_to "discussions" }
+  after_destroy_commit -> { broadcast_remove_to "discussions" }
+
   def to_param
     "#{id}-#{name.downcase.to_s[0...100]}".parameterize
   end
