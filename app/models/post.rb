1d06d4184f6235c07e3b577f420df3de00f22c77
Andrea Fomera <afomera@hey.com>
2021-03-06 16:34:14 -0700

    Realtime creation of posts for everyone on the discussion page

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,6 @@ class Post < ApplicationRecord
   has_rich_text :body
 
   validates :body, presence: true
+
+  after_create_commit -> { broadcast_append_to discussion, partial: "discussions/posts/post", locals: { post: self }}
 end
