eedcc2963a7bc6f6dce5dc29d02ad30abcb6f66a
Andrea Fomera <afomera@hey.com>
2021-03-27 15:43:04 -0600

    Add realtime moving of categories

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -36,6 +36,21 @@ class DiscussionsController < ApplicationController
       if @discussion.update(discussion_params)
         @discussion.broadcast_replace(partial: "discussions/header", locals: { discussion: @discussion })
 
+        if @discussion.saved_change_to_category_id?
+          old_category_id, new_category_id = @discussion.saved_change_to_category_id
+
+          old_category = Category.find(old_category_id)
+          new_category = Category.find(new_category_id)
+
+          # remove it from the old category list / insert to new list
+          @discussion.broadcast_remove_to(old_category)
+          @discussion.broadcast_prepend_to(new_category)
+
+          # Update categories by replacing them. This updates the counters in the sidebar.
+          old_category.reload.broadcast_replace_to("categories")
+          new_category.reload.broadcast_replace_to("categories")
+        end
+
         format.html { redirect_to @discussion, notice: "Discussion updated" }
       else
         format.html { render :edit, status: :unprocessable_entity }
