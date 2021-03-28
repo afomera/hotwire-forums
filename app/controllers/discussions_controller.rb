21aedea0c0c95cd64eaa71802553abceb821b22c
Andrea Fomera <afomera@hey.com>
2021-03-27 18:05:18 -0600

    reduce n+1 queries on Discussion show

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -7,7 +7,7 @@ class DiscussionsController < ApplicationController
   end
 
   def show
-    @posts = @discussion.posts.all.order(created_at: :asc)
+    @posts = @discussion.posts.includes(:user, :rich_text_body).order(created_at: :asc)
     @new_post = @discussion.posts.new
   end
 
