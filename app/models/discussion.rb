d33a7e96876860e6f2e0e4c6952974c473f32f01
Andrea Fomera <afomera@hey.com>
2021-03-28 13:56:17 -0600

    Display subscription status in UI

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -49,4 +49,30 @@ class Discussion < ApplicationRecord
       discussion_subscriptions.create(user: user, subscription_type: "optin")
     end
   end
+
+  def subscribed?(user)
+    return false if user.nil?
+
+    if subscription = subscription_for(user)
+      subscription.subscription_type == "optin"
+    else
+      posts.where(user_id: user.id).any?
+    end
+  end
+
+  def subscribed_reason(user)
+    return "You're not receiving notifications from this thread" if user.nil?
+
+    if subscription = subscription_for(user)
+      if subscription.subscription_type == "optout"
+        "You're ignoring this thread."
+      elsif subscription.subscription_type == "optin"
+        "You're receiving notifications because you've subscribed to this thread."
+      end
+    elsif posts.where(user_id: user.id).any?
+      "You're receiving notifications because you've posted in this thread."
+    else
+      "You're not receiving notifications from this thread"
+    end
+  end
 end
